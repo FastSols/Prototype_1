efb8a54cdb24b3389f5738e78940165cb77cc96d
Chinmay Chitale <chitalechinmay@outlook.com>
2018-07-05 15:46:33 +0530

    FIrst Commit

---
+++ App3/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android;
+using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace App3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.content_main);
+
+        }
+
+
+    }
+}
+
