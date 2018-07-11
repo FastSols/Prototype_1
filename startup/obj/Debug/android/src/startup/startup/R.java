2e9023c048b755565c6753ca579e5ed07ab88bad
chinmay chitale <chitalechinmay@outlook.com>
2018-07-12 01:59:29 +0530

    Added about us page yet to be completed

--- startup/obj/Debug/android/src/startup/startup/R.java
+++ startup/obj/Debug/android/src/startup/startup/R.java
@@ -23,12 +23,13 @@ public final class R {
         public static final int textview=0x7f040006;
     }
     public static final class layout {
-        public static final int answerpage=0x7f020000;
-        public static final int dashboard=0x7f020001;
-        public static final int main=0x7f020002;
-        public static final int search=0x7f020003;
-        public static final int signin=0x7f020004;
-        public static final int signup=0x7f020005;
+        public static final int aboutus=0x7f020000;
+        public static final int answerpage=0x7f020001;
+        public static final int dashboard=0x7f020002;
+        public static final int main=0x7f020003;
+        public static final int search=0x7f020004;
+        public static final int signin=0x7f020005;
+        public static final int signup=0x7f020006;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
