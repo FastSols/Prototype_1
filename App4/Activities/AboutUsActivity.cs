869f49f221f4c4e6b68ff825370402215119b19a
Sudershan Shivjatak <sudershan22@gmail.com>
2018-07-28 19:26:49 +0530

    Ssup, This is the updated webview

--- App4/Activities/AboutUsActivity.cs
+++ App4/Activities/AboutUsActivity.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Webkit;
 
 namespace App4.Activities
 {
@@ -17,9 +18,15 @@ namespace App4.Activities
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            WebView web_view;
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.AboutUs);
             // Create your application here
+
+            web_view = FindViewById<WebView>(Resource.Id.webView);
+            web_view.Settings.JavaScriptEnabled = true;
+            //  web_view.SetWebViewClient(new HelloWebViewClient())
+            web_view.LoadUrl("http://www.instantsolution.co.in/#team");
         }
     }
 }
